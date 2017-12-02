039889d79dcb1b23df3836333c6c24716898693e
Christoph Wedenig <christoph@wedenig.org>
2017-12-02 16:24:11 +0100

    solved day2/part1

---
+++ day2/part1.py
@@ -0,0 +1,14 @@
+file = open("day2/input.txt", "r")
+input = file.read().splitlines()
+sum = 0
+
+print("AoC 2017 - Day 2 / Part 1")
+
+for line in input:
+    # print(line)
+    row = [ int(x) for x in line.split('\t') ]
+    checksum = max(row) - min(row)
+    sum = sum + checksum;
+    pass
+
+print(sum)
