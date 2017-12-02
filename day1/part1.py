c1ef630c79c9d31b61487545200c3fb4d726a2b2
Christoph Wedenig <christoph@wedenig.org>
2017-12-02 18:16:33 +0100

    removed prints

--- day1/part1.py
+++ day1/part1.py
@@ -1,14 +1,12 @@
-file = open("./input.txt", "r")
+file = open("day1/input.txt", "r")
 nums = list(file.read())
 sum = 0
 
 nums[len(nums)-1] = nums[0]; # let the array wrap
 
 for i in range(0, len(nums) ):
-    # print("nums["+str(i)+"] >> "+nums[i])
     if i + 1 < len(nums):
         if nums[i] == nums[i + 1]:
-            print(nums[i])
             sum = sum + int(nums[i])
     pass
 
