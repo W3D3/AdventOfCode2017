173107664f588c472f848e13b0f60b084087c95a
Christoph Wedenig <christoph@wedenig.org>
2017-12-02 18:15:43 +0100

    removed unneeded lines

--- day1/part2.py
+++ day1/part2.py
@@ -1,18 +1,13 @@
 def matchingIndex(i, length):
     return int((i + length / 2) % length)
 
-file = open("./input.txt", "r")
+file = open("day1/input.txt", "r")
 nums = list(file.read())
 sum = 0
 nums.pop() # remove linebreak
 
-nums[len(nums)-1] = nums[0]; # let the array wrap
-print(len(nums))
 for i in range(0, len(nums) ):
-    print("nums["+str(i)+"] >> "+nums[i])
     if nums[i] == nums[matchingIndex(i, len(nums))]:
-        print(nums[i])
         sum = sum + int(nums[i])
     pass
-
 print(sum)
